cfff72d70860acdb0f84328b6061c6d7752488c5
Nikita Grebenyuk <nikich340@gmail.com>
2022-11-16 13:14:01 +1000

    + NR_LocalizedStringStorage: Add custom test class

--- WolvenKit.CR2W/Types/W3/RTTIConvert/CItemEntity.cs
+++ WolvenKit.CR2W/Types/W3/RTTIConvert/CItemEntity.cs
@@ -24,4 +24,23 @@ namespace WolvenKit.CR2W.Types
 		public override void Write(BinaryWriter file) => base.Write(file);
 
 	}
+
+    // nikich340 debug: REMOVE!
+    public class NR_LocalizedStringStorage : CEntity
+    {
+        [Ordinal(1)] [RED("stringValues")] public CArray<LocalizedString> stringValues { get; set; }
+
+        [Ordinal(2)] [RED("stringKeys")] public CArray<CString> stringKeys { get; set; }
+
+        [Ordinal(2)] [RED("stringIds")] public CArray<CInt32> stringIds { get; set; }
+
+        public NR_LocalizedStringStorage(CR2WFile cr2w, CVariable parent, string name) : base(cr2w, parent, name) { }
+
+        public static new CVariable Create(CR2WFile cr2w, CVariable parent, string name) => new NR_LocalizedStringStorage(cr2w, parent, name);
+
+        public override void Read(BinaryReader file, uint size) => base.Read(file, size);
+
+        public override void Write(BinaryWriter file) => base.Write(file);
+
+    }
 }
